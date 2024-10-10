51ecd7969a1bf799cb2390708942561e9bddba30
omaralmhammed <almahammedomar0.com>
2024-10-10 23:05:29 +0300

    scafullding

---
+++ newProjectSUHA.Server/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace newProjectSUHA.Server.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public string? Image { get; set; }
+
+    public virtual ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+    public virtual Category? Category { get; set; }
+
+    public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
