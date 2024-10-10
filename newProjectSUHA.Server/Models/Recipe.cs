2ab97977738babf937b5d8f99f55fea2241da1a0
Batool-Khazali <batoul.khazali@hotmail.com>
2024-10-10 23:07:49 +0300

    test - B

---
+++ newProjectSUHA.Server/Models/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace newProjectSUHA.Server.Models;
+
+public partial class Recipe
+{
+    public int Id { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Image { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? NutritionalFacts { get; set; }
+
+    public virtual RecipesCategory? Category { get; set; }
+}
