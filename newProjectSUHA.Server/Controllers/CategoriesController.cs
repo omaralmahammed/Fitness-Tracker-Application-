2431c8a85c0fe11304683348caf90640eafb480d
Esraa Odat <esraaodat123123@gmail.com>
2024-10-10 23:25:42 +0300

    esraa

--- newProjectSUHA.Server/Controllers/CategoriesController.cs
+++ newProjectSUHA.Server/Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using newProjectSUHA.Server.Dtos;
 using newProjectSUHA.Server.Models;
 
 namespace newProjectSUHA.Server.Controllers
