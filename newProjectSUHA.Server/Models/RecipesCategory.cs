659b9fb72950e1bff00c091803bb0f6affb71f23
qusai_sn <qusai_sn@outlook.com>
2024-10-10 23:06:36 +0300

    qusai first commit

---
+++ newProjectSUHA.Server/Models/RecipesCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace newProjectSUHA.Server.Models;
+
+public partial class RecipesCategory
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public virtual ICollection<Recipe> Recipes { get; set; } = new List<Recipe>();
+}
--- newProjectSUHA.Server/Program.cs
+++ newProjectSUHA.Server/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add services to the container.
+//builder.Services.AddDbContext<MyDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("YourConnectionString")));
+
+
 
 var app = builder.Build();
 
