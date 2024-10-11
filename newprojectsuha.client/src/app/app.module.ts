0b2b77f7501331c21619cad3a0f25ed74550d52f
SuhaKrasneh <suha.kras967@gmail.com>
2024-10-11 22:02:55 +0300

    done

--- newprojectsuha.client/src/app/app.module.ts
+++ newprojectsuha.client/src/app/app.module.ts
@@ -30,7 +30,7 @@ import { TipsComponent } from './tips/tips.component';
     GymAndClassItemsComponent,
     ItemDetailsComponent,
     GymAndClassSubscriptionComponent,
-    PreLoaderComponent
+    PreLoaderComponent,
     GymAndClassItemsComponent,
     TipsComponent
   ],
@@ -45,7 +45,7 @@ import { TipsComponent } from './tips/tips.component';
       { path: "gym", component: GymComponent },
       { path: "GymAndClassItems/:type", component: GymAndClassItemsComponent },
       { path: "itemDetails/:id", component: ItemDetailsComponent },
-      { path: "subscriptions/:id", component: GymAndClassSubscriptionComponent }
+      { path: "subscriptions/:id", component: GymAndClassSubscriptionComponent },
       { path: "Tips", component: TipsComponent }
 
     ])
