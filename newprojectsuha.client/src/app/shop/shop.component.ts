ee8beb68e7ff570ceb2924959a65d238c1d3a228
Esraa Odat <esraaodat123123@gmail.com>
2024-10-11 18:09:13 +0300

    The Shop Page is complete

--- newprojectsuha.client/src/app/shop/shop.component.ts
+++ newprojectsuha.client/src/app/shop/shop.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UrlService } from '../URL-Service/url.service';
 
 @Component({
   selector: 'app-shop',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrl: './shop.component.css'
 })
 export class ShopComponent {
+  allProductsArray: any[] = []; // لتخزين جميع المنتجات
+  allCategoriesArray: any[] = []; // لتخزين جميع الفئات
+  selectedCategory: string = ''; // الفئة المحددة
 
+  constructor(private productService: UrlService) { }
+
+  ngOnInit() {
+    // جلب الفئات
+    this.productService.GetAllCategories().subscribe((categories) => {
+      this.allCategoriesArray = categories;
+    });
+
+    // جلب جميع المنتجات بشكل افتراضي
+    this.productService.GetAllProducts().subscribe((products) => {
+      this.allProductsArray = products;
+    });
+  }
+
+  // تغيير الفئة المحددة عند اختيار فئة جديدة
+  onFilterChanged(categoryId: string) {
+    this.selectedCategory = categoryId;
+    this.filterProductsByCategory(categoryId);
+  }
+
+  // تصفية المنتجات بناءً على الفئة المحددة
+  filterProductsByCategory(categoryId: string) {
+    if (categoryId) {
+      this.productService.GetProductsByCategory(categoryId).subscribe((products) => {
+        this.allProductsArray = products;
+      });
+    } else {
+      // إذا لم يتم اختيار فئة، اعرض جميع المنتجات
+      this.productService.GetAllProducts().subscribe((products) => {
+        this.allProductsArray = products;
+      });
+    }
+  }
 }
