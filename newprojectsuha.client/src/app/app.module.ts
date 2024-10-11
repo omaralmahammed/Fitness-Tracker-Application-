09ca7834e0a4cbd5028b7b2a7235782977e018a1
tuqahrahsheh <tuqahrahsheh00@gmail.com>
2024-10-11 19:44:00 +0300

    create the login page

--- newprojectsuha.client/src/app/app.module.ts
+++ newprojectsuha.client/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- Add FormsModule here
+import { HttpClientModule } from '@angular/common/http'; // <-- Import HttpClientModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './suha/home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
-import { RouterModule } from '@angular/router';
 import { RegisterComponent } from './Tuqa/register/register.component';
 import { ShopComponent } from './shop/shop.component';
 import { GymComponent } from './gym/gym.component';
@@ -15,6 +15,7 @@ import { GymAndClassItemsComponent } from './gym-and-class-items/gym-and-class-i
 import { ItemDetailsComponent } from './item-details/item-details.component';
 import { GymAndClassSubscriptionComponent } from './gym-and-class-subscription/gym-and-class-subscription.component';
 import { PreLoaderComponent } from './pre-loader/pre-loader.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -23,9 +24,7 @@ import { PreLoaderComponent } from './pre-loader/pre-loader.component';
     NavBarComponent,
     FooterComponent,
     RegisterComponent,
-    FooterComponent,
     ShopComponent,
-    FooterComponent,
     GymComponent,
     GymAndClassItemsComponent,
     ItemDetailsComponent,
@@ -33,19 +32,18 @@ import { PreLoaderComponent } from './pre-loader/pre-loader.component';
     PreLoaderComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
+    BrowserModule,
+    HttpClientModule,  // <-- Ensure HttpClientModule is imported
+    FormsModule,       // <-- Add FormsModule here to enable ngModel and ngForm
     AppRoutingModule,
     RouterModule.forRoot([
       { path: "", component: HomeComponent, pathMatch: "full" },
       { path: "Shop", component: ShopComponent },
-      { path: "gym", component: GymComponent},
+      { path: "gym", component: GymComponent },
       { path: "GymAndClassItems/:type", component: GymAndClassItemsComponent },
       { path: "itemDetails/:id", component: ItemDetailsComponent },
-      { path: "subscriptions/:id", component: GymAndClassSubscriptionComponent },
-
+      { path: "subscriptions/:id", component: GymAndClassSubscriptionComponent }
     ])
-
-
   ],
   providers: [],
   bootstrap: [AppComponent]
