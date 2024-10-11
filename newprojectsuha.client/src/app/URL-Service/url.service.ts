61639f509f9b28325613049469dc9c6a0b5316ac
Batool-Khazali <batoul.khazali@hotmail.com>
2024-10-11 22:00:19 +0300

    errors in module

--- newprojectsuha.client/src/app/URL-Service/url.service.ts
+++ newprojectsuha.client/src/app/URL-Service/url.service.ts
@@ -32,6 +32,22 @@ export class UrlService {
     return this.http.get<any>(`https://localhost:7286/api/Nutirition/Tips`)
 
   }
+
+
+  GetGymAndClassItemDetails(id: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}GymAndClass/GetItemsDetails/${id}`)
+  }
+
+  GetGymAndClassItemAvailableTime(id: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}GymAndClass/GetAvailableTime/${id}`)
+  }
+
+  GetSubscriptions(id: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}GymAndClass/GetSubscription/${id}`)
+  }
+
+
+
 }
 
 
