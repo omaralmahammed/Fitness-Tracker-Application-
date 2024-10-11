f0373a93c944ef4ab9dc0ca3d62b25d6230d2cf2
Batool-Khazali <batoul.khazali@hotmail.com>
2024-10-11 19:38:14 +0300

    made preloader component and added it to the app.component.html file

--- newprojectsuha.client/src/app/app.module.ts
+++ newprojectsuha.client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { GymComponent } from './gym/gym.component';
 import { GymAndClassItemsComponent } from './gym-and-class-items/gym-and-class-items.component';
 import { ItemDetailsComponent } from './item-details/item-details.component';
 import { GymAndClassSubscriptionComponent } from './gym-and-class-subscription/gym-and-class-subscription.component';
+import { PreLoaderComponent } from './pre-loader/pre-loader.component';
 
 @NgModule({
   declarations: [
@@ -21,14 +22,15 @@ import { GymAndClassSubscriptionComponent } from './gym-and-class-subscription/g
     HomeComponent,
     NavBarComponent,
     FooterComponent,
-    RegisterComponent
+    RegisterComponent,
     FooterComponent,
     ShopComponent,
     FooterComponent,
     GymComponent,
     GymAndClassItemsComponent,
     ItemDetailsComponent,
-    GymAndClassSubscriptionComponent
+    GymAndClassSubscriptionComponent,
+    PreLoaderComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
