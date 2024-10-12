666e1d0fccc92a67289bf789a2a13b23b2fb8457
Esraa Odat <esraaodat123123@gmail.com>
2024-10-12 20:15:46 +0300

    esraa

--- newprojectsuha.client/src/app/Tuqaa/contact-us/contact-us.component.ts
+++ newprojectsuha.client/src/app/Tuqaa/contact-us/contact-us.component.ts
@@ -1,43 +1,43 @@
-//import { Component } from '@angular/core';
-//import { UrlService } from '../../URL-Service/url.service';
-//import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { UrlService } from '../../URL-Service/url.service';
+import { Router } from '@angular/router';
 
-//@Component({
-//  selector: 'app-contact-us',
-//  templateUrl: './contact-us.component.html',
-//  styleUrls: ['./contact-us.component.css']
-//})
-//export class ContactUsComponent {
+@Component({
+  selector: 'app-contact-us',
+  templateUrl: './contact-us.component.html',
+  styleUrls: ['./contact-us.component.css']
+})
+export class ContactUsComponent {
 
-//  // This is your form model
-//  contactForm = {
-//    name: '',
-//    email: '',
-//    phone: '',
-//    subject: '',
-//    message: ''
-//  };
+  // This is your form model
+  contactForm = {
+    name: '',
+    email: '',
+    phone: '',
+    subject: '',
+    message: ''
+  };
 
-//  constructor(private _ser: UrlService, private _router: Router) { }
+  constructor(private _ser: UrlService, private _router: Router) { }
 
-//  ngOnInit() { }
+  ngOnInit() { }
 
-//  // This will submit the form data
-//  submitContactForm() {
-//    const form = new FormData();
+  // This will submit the form data
+  submitContactForm(data: any) {
+    const form = new FormData();
 
-//    // Loop over the contactForm object and append the data
-//    for (let key in this.contactForm) {
-//      form.append(key, this.contactForm[key]);
-//    }
+    // Append form fields dynamically to FormData
+    for (let key in data) {
+      form.append(key, data[key])
+    }
 
-//    // Call the UrlService to submit the form
-//    this._ser.submitContact(form).subscribe(() => {
-//      alert("Contact form submitted successfully");
-//      this._router.navigate(['/thank-you-page']); // Replace with your desired route
-//    },
-//      (error) => {
-//        alert("Error submitting the contact form: " + error.error);
-//      });
-//  }
-//}
+    this._ser.submitContact(form).subscribe(() => {
+      alert('Your message has been successfully sent! We will get back to you soon.');
+      this._router.navigate(['/LogIn'])
+    },
+      (error) => {
+        alert(error.error)
+      })
+  }
+}
+
