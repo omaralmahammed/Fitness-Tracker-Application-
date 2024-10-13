11a9f360bed248cc1b740c9c16c879b6206c89fb
qusai_sn <qusai_sn@outlook.com>
2024-10-13 17:42:11 +0300

    create

--- newprojectsuha.client/src/app/suha/admin/GymAndClasses/create/create.component.ts
+++ newprojectsuha.client/src/app/suha/admin/GymAndClasses/create/create.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { UrlService } from '../../../../URL-Service/url.service';  // Import the service
+import { Router } from '@angular/router';  // Import Router for navigation
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrl: './create.component.css'
+  styleUrls: ['./create.component.css']  
 })
 export class CreateComponent {
+  newItem: any = {
+    name: '',
+    trainer: '',
+    description: '',
+    price: null,
+    flag: '',   
+    image: ''
+  };  // Property to hold the new item data
 
+  constructor(private urlService: UrlService, private router: Router) { }
+
+  createItem(): void {
+    this.urlService.CreateClassAndGym(this.newItem)  // Call the service to create a new item
+      .subscribe({
+        next: (response) => {
+          console.log('Item created successfully:', response);
+          this.router.navigate(['/display']);  // Navigate back to the display page after creation
+        },
+        error: (error) => {
+          console.error('Error creating item:', error);
+        }
+      });
+  }
 }
