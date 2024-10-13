3a894a20b6902cb434146bb456f3f981316c3caa
DuhaOdat <odatduha@gmail.com>
2024-10-13 09:40:07 +0300

    add testimonial

--- newprojectsuha.client/src/app/URL-Service/url.service.ts
+++ newprojectsuha.client/src/app/URL-Service/url.service.ts
@@ -105,6 +105,11 @@ export class UrlService {
     return this.http.get<any>(`${this.baseUrl}Testimonials`);
 
   }
+  addTestimonial(formData: FormData): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}Testimonials`, formData)
+  }
+
+  
 
 
   getCartItems(id: number): Observable<any> {
