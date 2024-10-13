8a4d06fb39c5d42cfb4d4383babff3964599a761
Esraa Odat <esraaodat123123@gmail.com>
2024-10-13 11:11:09 +0300

    j

--- newProjectSUHA.Server/Controllers/PymentController.cs
+++ newProjectSUHA.Server/Controllers/PymentController.cs
@@ -1,4 +1,4 @@
-﻿/*using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using newProjectSUHA.Server.Dtos;
@@ -78,4 +78,3 @@ namespace newProjectSUHA.Server.Controllers
         }
     }
 }
-*/
